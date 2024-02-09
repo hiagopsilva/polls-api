bc5095f42ccc184bb46f9b7a808e25082f2c8599
Hiago Prates <hiagohps360@gmail.com>
2024-02-08 23:09:14 -0300

    chore: pub sub votes

--- src/ws/poll-results.ts
+++ src/ws/poll-results.ts
@@ -1,9 +1,17 @@
 import { FastifyInstance } from "fastify";
+import { voting } from "../utils/voting-pub-sub";
+import { z } from "zod";
 
 export async function pollResults(app: FastifyInstance) {
   app.get('/polls/:pollId/results',  { websocket: true }, (connection, request) => {
-    connection.socket.on('message', async (message: string) => {
-      connection.socket.send('you send: ' + message)
+    const getPollParams = z.object({
+      pollId: z.string().uuid(),
+    })
+
+    const { pollId } = getPollParams.parse(request.params)
+
+    voting.subscribe(pollId, (message) => {
+      connection.socket.send(JSON.stringify(message))
     })
   })
 }
