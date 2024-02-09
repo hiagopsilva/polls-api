516860717d04456be6fac919f17a04e201f44e21
Hiago Prates <hiagohps360@gmail.com>
2024-02-08 22:31:46 -0300

    chore: list votes

--- src/routes/get-poll.ts
+++ src/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { z } from "zod"
 import { prisma } from "../lib/prisma"
 import { FastifyInstance } from "fastify"
+import { redis } from "../lib/redis"
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -24,6 +25,27 @@ export async function getPoll(app: FastifyInstance) {
      },
     })
 
-    return reply.status(201).send(poll)
+    if (!poll) {
+      return reply.status(400).send({
+        message: 'Poll not found.'
+      })
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    console.log(result)
+
+    const votes = result.reduce((obj, line, index) => {
+
+      if (index % 2 === 0) {
+        const score = result[index + 1]
+
+        Object.assign(obj, { [line]: Number(score)})
+      }
+
+      return obj
+    }, {} as Record<string, number>)
+
+    return reply.status(201).send({votes, poll})
   })
 }
